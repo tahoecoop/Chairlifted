e7c9f3608ff0ebd0415f71198bba43a80bd97e82
tahoecoop <username@users.noreply.github.com>
2015-08-17 10:36:41 -0700

    Initial Commit

---
+++ Chairlifted/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Chairlifted
+//
+//  Created by Benjamin COOPER on 8/17/15.
+//  Copyright (c) 2015 EBB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
