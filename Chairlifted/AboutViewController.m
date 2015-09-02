85bb05d508dd4fbe88a4fc208be74b3b6e80bd3a
tahoecoop <username@users.noreply.github.com>
2015-09-02 10:58:44 -0700

    About screen

---
+++ Chairlifted/AboutViewController.m
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.m
+//  Chairlifted
+//
+//  Created by Benjamin COOPER on 9/2/15.
+//  Copyright (c) 2015 EBB. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onCloseButtonPressed:(UIBarButtonItem *)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
